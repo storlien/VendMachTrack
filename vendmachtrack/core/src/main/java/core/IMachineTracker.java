5cd91f0750bdffd931e55c44ae853d1d7d9e608e
Anette Lund <anettewl@stud.ntnu.no>
2023-09-20 19:31:37 +0200

    Issue 8: added javadocs, added setMachine

--- vendmachtrack/core/src/main/java/core/IMachineTracker.java
+++ vendmachtrack/core/src/main/java/core/IMachineTracker.java
@@ -4,8 +4,31 @@ import java.util.List;
 
 public interface IMachineTracker{
 
+    /**
+     * remove the vendingmachine from tracker
+	 * @param v  
+	 */
     public void removeVendingMachine(IVendingMachine v);
+
+    /**
+     * add the vendingmachine to tracker
+	 * @param v 
+	 */
     public void addVendingMachine(IVendingMachine v);
+
+
+    /**
+	 * 
+	 * @return the list of machines 
+	 */
     public List<IVendingMachine> getMachines();
+
+
+    /**
+     * 
+	 * sets the machine
+     * @param machines 
+	 */
+    public void setMachines(List<IVendingMachine> machines);
  
 }
