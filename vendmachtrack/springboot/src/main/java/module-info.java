63303fc017fb650980e09d7f3ed17cb1687c0947
Edward Storlien <carlst@stud.ntnu.no>
2023-10-19 13:58:17 +0200

    Issue #50: Remove Spring Boot parent from root POM. Add dependency management for Spring Boot Framework

--- vendmachtrack/springboot/src/main/java/module-info.java
+++ vendmachtrack/springboot/src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module vendmachtrack.springboot {
-    requires spring.beans;
     requires spring.boot;
-    requires spring.context;
     requires spring.boot.autoconfigure;
     requires spring.core;
+    //    requires spring.context;
+    //    requires spring.beans;
 
     // opens springboot to spring.beans, spring.context, spring.web, spring.core;
-
     opens springboot to spring.core;
 
 }
