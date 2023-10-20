012b489a390fccedd8c16acb2d6603c9b389449c
Alexander Grannes <alexander.grannes@gmail.com>
2023-10-20 22:45:16 +0200

    #52: Make Tests for PasswordHandler class

---
+++ vendmachtrack/core/src/test/java/core/PasswordHandlerTest.java
@@ -0,0 +1,37 @@
+package core;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+public class PasswordHandlerTest {
+    
+    
+  @Test
+    public void testHashPasswordWithRegularInput() {
+        String password = "normalPassword";
+        String hashedPassword = PasswordHandler.hashPassword(password);
+
+        assertNotNull(hashedPassword);
+        assertFalse(hashedPassword.isEmpty());
+        
+    }
+
+    @Test
+    public void testVerifyPasswordWithCorrectPassword() {
+        // need to know the correct password to test this
+    }
+
+    @Test
+    public void testVerifyPasswordWithIncorrectPassword() {
+        String password = "wrongPassword";
+        assertFalse(PasswordHandler.verifyPassword(password));
+    }
+
+
+}
