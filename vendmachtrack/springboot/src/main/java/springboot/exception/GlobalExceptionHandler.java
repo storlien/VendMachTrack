a7c50ed1909431321ad6407fac41a5e77ece5423
Edward Storlien <carlst@stud.ntnu.no>
2023-10-22 15:47:06 +0200

    Issue #51: Add exception for illegal input
    IllegalInputException can be thrown by service if input is invalid. The server then responds with a HTTP status code of BAD REQEUST.

--- vendmachtrack/springboot/src/main/java/springboot/exception/GlobalExceptionHandler.java
+++ vendmachtrack/springboot/src/main/java/springboot/exception/GlobalExceptionHandler.java
@@ -17,4 +17,10 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(IllegalInputException.class)
+    public ResponseEntity<?> illegalInputException(IllegalInputException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
 }
