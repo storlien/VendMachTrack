c15ae360ddb2e0a39bc40cbafc0fb0a94ba5f3fe
Alexander Grannes <alexander.grannes@gmail.com>
2023-10-23 10:47:07 +0200

    Issue #52: Make test for Passwordhandler class.  made a test that verifies password using the correct password. Right  now the password is written in plain text, witch migth not be good  security. rity.

--- vendmachtrack/core/src/test/java/core/PasswordHandlerTest.java
+++ vendmachtrack/core/src/test/java/core/PasswordHandlerTest.java
@@ -12,6 +12,9 @@ import org.mockito.Mockito;
 public class PasswordHandlerTest {
     
     
+/**
+ * Tests the hashPassword method of the PasswordHandler class with regular input.
+ */
   @Test
     public void testHashPasswordWithRegularInput() {
         String password = "normalPassword";
@@ -21,11 +24,22 @@ public class PasswordHandlerTest {
         assertFalse(hashedPassword.isEmpty()); 
     }
 
+    /**
+     * This test verifies that the PasswordHandler class can correctly verify a password that has been hashed.
+     * It sets a password, hashes it, and then verifies it using the verifyPassword method of the PasswordHandler class.
+     * If the password is verified, the test passes.
+     * 
+     */
     @Test
     public void testVerifyPasswordWithCorrectPassword() {
-        
+        String password = "brus"; // migth not be good security to have password in clear text
+        String hashedPassword = PasswordHandler.hashPassword(password);
+        assertTrue(PasswordHandler.verifyPassword(password)); 
     }
 
+    /**
+     * Tests the verifyPassword method of the PasswordHandler class with an incorrect password.
+     */
     @Test
     public void testVerifyPasswordWithIncorrectPassword() {
         String password = "wrongPassword";
