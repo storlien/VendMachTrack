0829711e15cc8ec85ab28b1fc4f25f32eec85478
Edward Storlien <carlst@stud.ntnu.no>
2023-10-24 13:20:56 +0200

    Issue #56: Add comments to Access interface and Remote Access class

--- vendmachtrack/ui/src/main/java/ui/access/MachineTrackerAccessRemote.java
+++ vendmachtrack/ui/src/main/java/ui/access/MachineTrackerAccessRemote.java
@@ -16,6 +16,10 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
+/**
+ * Class for accessing Vending Machine Tracker through REST API.
+ * Accesses the Spring Boot REST API server by following the documentation on possible requests to perform against the API.
+ */
 public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
 
     private final URI endpointBaseUri;
@@ -26,11 +30,21 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
     private final Type TYPE_HASHMAP_STRING_INTEGER = new TypeToken<HashMap<String, Integer>>() {
     }.getType();
 
+    /**
+     * Constructor. Requires a base URI for the REST API endpoint.
+     *
+     * @param endpointBaseUri The base URI of the REST API endpoint
+     */
     public MachineTrackerAccessRemote(URI endpointBaseUri) {
         this.endpointBaseUri = endpointBaseUri;
         this.gson = new Gson();
     }
 
+    /**
+     * Access method for the list of vending machines
+     *
+     * @return HashMap of vending machine list with vending machine ID as key and location as value
+     */
     @Override
     public HashMap<Integer, String> getVendMachList() {
         HttpRequest request = HttpRequest.newBuilder()
@@ -43,6 +57,12 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), TYPE_HASHMAP_INTEGER_STRING);
     }
 
+    /**
+     * Access method for the location of a vending machine
+     *
+     * @param id The ID of the vending machine
+     * @return Location of vending machine
+     */
     @Override
     public String getVendMachLocation(int id) {
         HttpRequest request = HttpRequest.newBuilder()
@@ -55,6 +75,12 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), String.class);
     }
 
+    /**
+     * Access method for inventory
+     *
+     * @param id The ID of the vending machine
+     * @return HashMap of inventory with item as key and amount as value
+     */
     @Override
     public HashMap<String, Integer> getInventory(int id) {
         HttpRequest request = HttpRequest.newBuilder()
@@ -67,6 +93,14 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), TYPE_HASHMAP_STRING_INTEGER);
     }
 
+    /**
+     * Access method for adding item to vending machine's inventory
+     *
+     * @param id     The ID of the vending machine
+     * @param item   The item name to be added
+     * @param amount The amount of the item to be added
+     * @return HashMap of inventory with item as key and amount as value
+     */
     @Override
     public HashMap<String, Integer> addItem(int id, String item, int amount) {
         String param1Value = URLEncoder.encode(String.valueOf(id), StandardCharsets.UTF_8);
@@ -88,6 +122,14 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), TYPE_HASHMAP_STRING_INTEGER);
     }
 
+    /**
+     * Access method for removing an amount of an item
+     *
+     * @param id     The ID of the vending machine
+     * @param item   The item to be removed
+     * @param amount The amount to be removed from the item
+     * @return HashMap of inventory with item as key and amount as value
+     */
     @Override
     public HashMap<String, Integer> removeItem(int id, String item, int amount) {
         String param1Value = URLEncoder.encode(String.valueOf(id), StandardCharsets.UTF_8);
@@ -109,6 +151,13 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), TYPE_HASHMAP_STRING_INTEGER);
     }
 
+    /**
+     * Access method for adding a new vending machine
+     *
+     * @param id       The ID of the new vending machine
+     * @param location The location of the new vending machine
+     * @return HashMap of vending machine list with vending machine ID as key and location as value
+     */
     @Override
     public HashMap<Integer, String> addVendMach(int id, String location) {
         String param1Key = "id";
@@ -129,6 +178,12 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), TYPE_HASHMAP_INTEGER_STRING);
     }
 
+    /**
+     * Access method for removing a vending machine
+     *
+     * @param id The ID of the vending machine
+     * @return HashMap of vending machine list with vending machine ID as key and location as value
+     */
     @Override
     public HashMap<Integer, String> removeVendMach(int id) {
 
@@ -143,6 +198,13 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), TYPE_HASHMAP_INTEGER_STRING);
     }
 
+    /**
+     * Access method for changing the location of a vending machine
+     *
+     * @param id       The ID of the vending machine
+     * @param location The new location of the vending machine
+     * @return HashMap of vending machine list with vending machine ID as key and location as value
+     */
     @Override
     public HashMap<Integer, String> changeLocation(int id, String location) {
         String param1Key = "location";
@@ -161,6 +223,12 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         return gson.fromJson(response.body(), TYPE_HASHMAP_INTEGER_STRING);
     }
 
+    /**
+     * Internal method for checking if the response was successful or not.
+     * If not, it will throw a RunTimeExceptionError with the message being the error message from the HTTP response.
+     *
+     * @param response HttpReponse to be checked
+     */
     private void checkError(HttpResponse<String> response) {
         if (response.statusCode() != HttpURLConnection.HTTP_OK) {
             JsonElement rootElement = JsonParser.parseString(response.body());
@@ -170,6 +238,12 @@ public class MachineTrackerAccessRemote implements IMachineTrackerAccess {
         }
     }
 
+    /**
+     * Internal method for sending the request and returning the response.
+     *
+     * @param request HttpRequest to be sent
+     * @return HttpReponse from the request sent
+     */
     private HttpResponse<String> getResponse(HttpRequest request) {
         try {
             return HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
