fd140f6a06b296da71bf3487ec78691819c28757
Anette Lund <anettewl@stud.ntnu.no>
2023-10-25 15:41:51 +0200

    Issue #58: Update refillController to make ui for refill-page
    Made the content to the refill controller- class. Created methods for updating the title on the refill- page based on the choicen machine, for setting the concrete machine-ID to the field- ID in this class. Also made methods to refill the inventory for the chosen machine.
    
    Co-Authored-By: Oda Bang-Olsen <odaban@stud.ntnu.no>

--- vendmachtrack/ui/src/main/java/vendmachtrack/ui/RefillController.java
+++ vendmachtrack/ui/src/main/java/vendmachtrack/ui/RefillController.java
@@ -1,19 +1,32 @@
 package vendmachtrack.ui;
 
-import vendmachtrack.core.Item;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import vendmachtrack.ui.access.MachineTrackerAccessRemote;
+import vendmachtrack.ui.access.MachineTrackerAccessible;
 
 public class RefillController {
 
     @FXML
-    private TextArea title;
+    private Label title;
 
     @FXML
-    private ChoiceBox<Item> choiceBox;
+    private TextField refillItem;
 
     @FXML
     private TextField refillNumber;
@@ -22,13 +35,91 @@ public class RefillController {
     private Button refillButton;
 
     @FXML
-    private TextArea textArea;
-
+    private Button back;
 
     @FXML
-    private void refillItem() {
+    private TextArea textArea;
+
+    @FXML
+    private Text answerText;
+
+    private MachineTrackerAccessible access;
+
+    private int selectedMachineID;
+
+    private Stage stage;
+    private Scene scene;
+
+    public void updateTitle(int machineID) {
+        title.setText("Vending machine: " + machineID);
+    }
+
+    public void setSelectedMachineID(int machineID) { // denne metoden sørger for at vi overfører den id-en som man er
+                                                      // på i controllerklassen hit
+        this.selectedMachineID = machineID;
+        updateTitle(machineID);
+    }
+
+    public void setMachineTrackerAccess(MachineTrackerAccessible access) {
+        this.access = new MachineTrackerAccessRemote(URI.create("http://localhost:8080/"));
 
     }
 
+    @FXML
+    private void refillItem() {
+        if (refillItem.getText() == null || !isLettersOnly(refillItem.getText())
+                || !isDigitsOnly(refillNumber.getText())) {
+            answerText.setText("Not a valid input");
+        }
+
+        HashMap<String, Integer> updatedInventory = new HashMap<>();
+
+        try {
+            updatedInventory = access.addItem(selectedMachineID, refillItem.getText(),
+                    Integer.parseInt(refillNumber.getText()));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            StringBuilder formattedStatus = new StringBuilder("Inventory:\n");
+            for (Map.Entry<String, Integer> entry : updatedInventory.entrySet()) {
+                formattedStatus.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            }
+            textArea.setText(formattedStatus.toString());
+
+        } catch (Exception e) {
+            textArea.setText(e.getMessage());
+        }
+
+        // her skal den oppdatere inventory for den maskinen og returnere det (den må
+        // selvfølgelig også oppdatere for inventory som kommer opp på forsiden)
+
+    }
+
+    @FXML
+    public void goBackToFirstPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static boolean isLettersOnly(String input) {
+        String regex = "^[a-zA-Z]+$";
+        Pattern pattern = Pattern.compile(regex);
+
+        return pattern.matcher(input).matches();
+
+    }
+
+    public static boolean isDigitsOnly(String input) {
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+
+        return pattern.matcher(input).matches();
+    }
 
 }
