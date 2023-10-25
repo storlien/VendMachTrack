dddf2053c28da7b129b6be3ad498149438f34f26
Oda <odaban@stud.ntnu.no>
2023-10-25 15:43:09 +0200

    Issue #70: Develop UserController and connect it to VendAppController
    Created functionality for the user view interface, and made it accesible
    fra the main view in our application. In addition, added a css class to
    update the appearace of the user interface.
    
    These changes works remote, but not locally. This is will be fixed in a new branch.
    
    Co-Authored-By: Anette Wiken Lund <anettewl@stud.ntnu.no>

--- vendmachtrack/ui/src/main/java/ui/UserController.java
+++ vendmachtrack/ui/src/main/java/ui/UserController.java
@@ -1,14 +1,32 @@
 package ui;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import ui.access.IMachineTrackerAccess;
+import ui.access.MachineTrackerAccessRemote;
 
 public class UserController {
 
+    @FXML
+    private Label mylabel;
 
     @FXML
-    private TextArea view;
+    private Pane pane;
 
     @FXML
     private Button backButton;
@@ -16,10 +34,80 @@ public class UserController {
     @FXML
     private Button buy;
 
-
+    @FXML
+    private VBox buttonContainer;
 
     @FXML
-        private void removeItem() {
+    private TextField chosenItem;
+
+    private IMachineTrackerAccess access;
+    private int selectedMachine;
+    private Stage stage;
+    private Scene scene;
+
+    public void updateButtons(int machineID, Map<String, Integer> inventory) {
+        buttonContainer.getChildren().clear(); // Clear existing buttons
+
+        VBox currentColumn = new VBox(); // Create a new column for buttons
+        currentColumn.setSpacing(20); // Set spacing between buttons
+
+        int buttonsInCurrentColumn = 0; // Counter for buttons in the current column
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            String itemName = entry.getKey();
+            int itemQuantity = entry.getValue();
+
+            Button button = new Button(itemName + ": " + itemQuantity);
+            button.getStyleClass().add("buttonContainer"); // Add CSS class to the button
+
+            button.setOnAction(e -> chosenItem.setText(itemName)); // Button event handling
+
+            currentColumn.getChildren().add(button); // Add the button to the current column
+            buttonsInCurrentColumn++;
+
+            if (buttonsInCurrentColumn >= 5) {
+                // If there are already 5 buttons in the column, add the column to
+                // buttonContainer and create a new column
+                buttonContainer.getChildren().add(currentColumn);
+                currentColumn = new VBox();
+                currentColumn.setSpacing(20); // Set spacing between buttons in the new column
+                buttonsInCurrentColumn = 0; // Reset the button counter for the new column
+            }
+        }
+
+        // Add the last remaining column if there are buttons in it
+        if (buttonsInCurrentColumn > 0) {
+            buttonContainer.getChildren().add(currentColumn);
+        }
+    }
+
+    @FXML
+    private void removeItem() {
+        HashMap<String, Integer> newInventory = access.removeItem(selectedMachine, chosenItem.getText(), 1);
+        updateButtons(selectedMachine, newInventory);
+
+    }
+
+    public void updateTitle(int machineID) {
+        mylabel.setText("Vending machine: " + machineID);
+    }
+
+    public void setSelectedMachineID(int machineID) { // denne metoden sørger for at vi overfører den id-en som man er
+                                                      // på i controllerklassen hit
+        this.selectedMachine = machineID;
+        System.out.println(selectedMachine);
+        updateTitle(selectedMachine);
+        access = new MachineTrackerAccessRemote(URI.create("http://localhost:8080/"));
+        updateButtons(selectedMachine, access.getInventory(selectedMachine));
+    }
+
+    @FXML
+    public void backToHomePage(ActionEvent event) throws IOException { // legge til at den må vise passord
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
 
--- vendmachtrack/ui/src/main/java/ui/VendAppController.java
+++ vendmachtrack/ui/src/main/java/ui/VendAppController.java
@@ -9,15 +9,21 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 import ui.access.IMachineTrackerAccess;
 import ui.access.MachineTrackerAccessRemote;
 
+import java.io.IOException;
 import java.net.URI;
 
 /**
@@ -36,19 +42,25 @@ public class VendAppController implements Initializable {
     private Button button;
 
     @FXML
-    private ChoiceBox<String> menuBar;
+    private Button userView;
 
+    @FXML
+    private ChoiceBox<String> menuBar;
 
     private IMachineTrackerAccess access;
 
+    private Stage stage;
+    private Scene scene;
 
     /**
      * Initializes the controller. It reads vending machine data from a JSON file.
      *
-     * @param arg0 The location used to resolve relative paths for the root object, or null if the location is not known.
-     * @param arg1 The resources used to localize the root object, or null if the root object was not localized.
+     * @param arg0 The location used to resolve relative paths for the root object,
+     *             or null if the location is not known.
+     * @param arg1 The resources used to localize the root object, or null if the
+     *             root object was not localized.
      */
-    
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         access = new MachineTrackerAccessRemote(URI.create("http://localhost:8080/"));
@@ -60,15 +72,16 @@ public class VendAppController implements Initializable {
                 machines.add("id: " + entry.getKey() + " (" + entry.getValue() + ")");
             }
             menuBar.getItems().addAll(machines);
-            
+
         } catch (Exception e) {
             textArea.setText(e.getMessage());
         }
-        
-        } 
+
+    }
 
     /**
-     * Handles the button click event. It displays the inventory of the selected vending machine in the text area.
+     * Handles the button click event. It displays the inventory of the selected
+     * vending machine in the text area.
      *
      * @param event The event representing the button click.
      */
@@ -82,25 +95,46 @@ public class VendAppController implements Initializable {
                 formattedStatus.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
             }
             textArea.setText(formattedStatus.toString());
-            
-        } 
-        else {
+
+        } else {
             textArea.setText("No vending machine selected");
         }
 
     }
 
-    private int findID(){
+    private int findID() {
         String selectedItem = menuBar.getValue();
         if (selectedItem != null) {
             int colonIndex = selectedItem.indexOf(":");
             int endIndex = selectedItem.indexOf("(");
             int id = Integer.parseInt(selectedItem.substring(colonIndex + 2, endIndex).trim());
             return id;
-        }
-        else {return 0;
+        } else {
+            System.out.println("No vending machine selected"); // Print a message for debugging
+            return 0;
         }
     }
 
+    @FXML
+    public void userViewScene(ActionEvent event) throws IOException {
+        int selectedMachineID = findID();
 
+        if (selectedMachineID != 0) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("UserApp.fxml"));
+            Parent root = loader.load();
+
+            UserController userController = loader.getController();
+            userController.setSelectedMachineID(selectedMachineID);
+
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            // Handle the case when no vending machine is selected, display an error message
+            // or handle it according to your application logic.
+            textArea.setText("No vending machine selected");
+        }
+    }
 }
