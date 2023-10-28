6c2857519143ccfd7c2cc29e20f00353b094123b
Edward Storlien <carlst@stud.ntnu.no>
2023-10-28 13:58:52 +0200

    Issue #41: Add or enhance existing comments of 'core' module

--- vendmachtrack/core/src/main/java/vendmachtrack/core/VendingMachine.java
+++ vendmachtrack/core/src/main/java/vendmachtrack/core/VendingMachine.java
@@ -3,25 +3,44 @@ package vendmachtrack.core;
 import java.util.HashMap;
 
 /**
- * Represents a vending machine with unique id, inventory, and location.
+ * Represents a vending machine with its essential attributes and behaviors.
+ * <p>
+ * The VendingMachine class captures the core characteristics of a vending machine including
+ * its unique identifier, location, and the inventory status. The inventory is managed using a
+ * hashmap where the items are keys and their quantities serve as values. The class provides
+ * methods to manipulate and query these attributes.
+ * </p>
  */
-
 public class VendingMachine {
 
+    /**
+     * A HashMap holding the inventory status of the vending machine.
+     */
     private HashMap<String, Integer> status = new HashMap<>();
+
+    /**
+     * A unique identifier for the vending machine.
+     */
     private int id;
+
+    /**
+     * The physical location of the vending machine.
+     */
     private String location;
 
+    /**
+     * Default constructor for VendingMachine.
+     */
     public VendingMachine() {
 
     }
 
     /**
-     * Initializes a new vending machine with the provided id, inventory and location.
+     * Initializes a new vending machine with the specified attributes.
      *
-     * @param id        The unique id of the vending machine.
-     * @param inventory A map containing the items in the vending machine and their quantities.
-     * @param location  The location where the vending machine is placed.
+     * @param id        The unique identifier for the vending machine.
+     * @param inventory A map representing the initial inventory status with item names as keys and their quantities as values.
+     * @param location  The physical location where the vending machine is placed.
      */
     public VendingMachine(int id, HashMap<String, Integer> inventory, String location) {
         this.status = new HashMap<>(inventory);
@@ -31,9 +50,9 @@ public class VendingMachine {
 
 
     /**
-     * Retrieves the current status of items in the vending machine.
+     * Returns a defensive copy of the vending machine's inventory status.
      *
-     * @return A HashMap containing item names as keys and their quantities as values.
+     * @return A map representing the inventory with item names as keys and their quantities as values.
      */
     public HashMap<String, Integer> getStatus() {
         return new HashMap<>(this.status);
@@ -41,16 +60,16 @@ public class VendingMachine {
 
 
     /**
-     * Retrieves the unique identifier of the vending machine.
+     * Gets the unique identifier of the vending machine.
      *
-     * @return The id of the vending machine.
+     * @return The identifier of the vending machine.
      */
     public int getId() {
         return this.id;
     }
 
     /**
-     * Retrieve the location of the vending machine.
+     * Retrieves the physical location of the vending machine.
      *
      * @return The location of the vending machine.
      */
@@ -60,10 +79,11 @@ public class VendingMachine {
 
 
     /**
-     * Adds a specified number of a particular item to the vending machine's inventory.
+     * Modifies the inventory by adding a specified quantity of an item.
+     * If the item does not exist in the inventory, it gets added; otherwise, its quantity is updated.
      *
-     * @param item   The name of the item to add.
-     * @param number The number of the item to add.
+     * @param item   The name of the item.
+     * @param number The quantity of the item to be added.
      */
     public void addItem(String item, int number) {
         if (!status.containsKey(item)) {
@@ -75,12 +95,13 @@ public class VendingMachine {
 
 
     /**
-     * Removes a specified number of a particular item from the vending machine's inventory (someone buys an item).
-     * If the quantity becomes zero, the item is no longer available in the vending machine.
+     * Modifies the inventory by removing a specified quantity of an item.
+     * If the remaining quantity becomes zero after removal, the item is removed from the inventory.
+     * If the item does not exist or the specified quantity to remove exceeds the available amount, no action is taken.
      *
-     * @param item   The name of the item to remove.
-     * @param number The number of items to remove.
-     * @throws IllegalArgumentException If the item is not found in the vending machine's inventory.
+     * @param item   The name of the item.
+     * @param number The quantity of the item to be removed.
+     * @return True if removal was successful, false otherwise.
      */
     public boolean removeItem(String item, int number) {
         Integer itemCount = status.get(item);
@@ -103,9 +124,9 @@ public class VendingMachine {
 
 
     /**
-     * Sets the inventory status of the vending machine.
+     * Updates the vending machine's inventory status.
      *
-     * @param status A HashMap containing item names as keys and their quantities as values.
+     * @param status A map representing the new inventory status.
      */
     public void setStatus(HashMap<String, Integer> status) {
         this.status = new HashMap<>(status);
@@ -113,24 +134,21 @@ public class VendingMachine {
 
 
     /**
-     * Sets the unique identifier of the vending machine.
+     * Updates the unique identifier of the vending machine.
      *
-     * @param id The id to set for the vending machine.
+     * @param id The new identifier for the vending machine.
      */
     public void setId(int id) {
         this.id = id;
     }
 
     /**
-     * Sets the location of the vending machine.
+     * Sets the physical location of the vending machine.
      *
-     * @param location The location to set for the vending machine.
+     * @param location The new location for the vending machine.
      */
     public void setLocation(String location) {
         this.location = location;
     }
 
-
 }
-
-
