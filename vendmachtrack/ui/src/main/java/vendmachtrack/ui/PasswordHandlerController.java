00183a407fa5cd04765eb9468267e17cf5dc67ea
Oda <odaban@stud.ntnu.no>
2023-10-28 14:35:18 +0200

    Issue #76: Create PasswordHandlerController
    Developed a new controller class for the interface responsible for verifying the entered password. Upon successful validation, the main scene is displayed. Additionally, users have the option to return to the user view from this scene.

---
+++ vendmachtrack/ui/src/main/java/vendmachtrack/ui/PasswordHandlerController.java
@@ -0,0 +1,85 @@
+package vendmachtrack.ui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import vendmachtrack.core.PasswordHandler;
+import vendmachtrack.ui.access.AccessService;
+
+public class PasswordHandlerController {
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button backToUserView;
+
+    private Stage stage;
+    private Scene scene;
+    private App mainApp;
+    private int selectedMachineID;
+    private AccessService service;
+
+    public void onConfirmButtonClicked(ActionEvent event) throws IOException {
+        String inputPassword = passwordField.getText();
+
+        if (PasswordHandler.verifyPassword(inputPassword)) {
+            backToHomePage(event);
+
+        } else {
+            label.getStyleClass().add("error-text");
+            label.setText("Incorrect password. Please try again");
+        }
+    }
+
+    public void setMainApp(App mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    public void setAccessService(AccessService service) {
+        this.service = service;
+    }
+
+    @FXML
+    public void switchToMainScene(ActionEvent event) throws IOException {
+        mainApp.switchToMainScene(selectedMachineID);
+    }
+
+    @FXML
+    public void backToHomePage(ActionEvent event) throws IOException {
+        mainApp.switchToMainScene(selectedMachineID);
+    }
+
+    public void setSelectedMachineID(int machineID) {
+        this.selectedMachineID = machineID;
+    }
+
+    public void switchBackToUserView(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UserApp.fxml"));
+        Parent root = fxmlLoader.load();
+        UserController userController = fxmlLoader.getController();
+        userController.setAccessService(service);
+        userController.setMainApp(mainApp);
+        userController.setSelectedMachineID(selectedMachineID);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
