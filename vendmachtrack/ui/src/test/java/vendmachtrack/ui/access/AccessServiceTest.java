2ee2df995a3eb31b9c4d071e585cdf17f18bbdd7
Alexander Grannes <alexander.grannes@gmail.com>
2023-10-31 14:50:48 +0100

    Issue #86: Add detailed javadocs to AccessServiceTest Class
    Added detailed javadocs describing what the Test class does and what the
    methods are ment to test.

--- vendmachtrack/ui/src/test/java/vendmachtrack/ui/access/AccessServiceTest.java
+++ vendmachtrack/ui/src/test/java/vendmachtrack/ui/access/AccessServiceTest.java
@@ -12,10 +12,47 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.http.Fault;
 
+
+
+/**
+ * This class contains unit tests for the {@link vendmachtrack.ui.access.AccessService} class, which is responsible for providing access to a vending machine tracker.
+ * The tests cover various scenarios, including healthy and unhealthy remote server states, and exception handling.
+ *
+ * <p>
+ * 
+ * The WireMock framework is used to simulate HTTP responses from a remote server and ensure the expected behavior of the AccessService.
+ * For More info about the library see: <a href="https://github.com/tomakehurst/wiremock" target="_blank">WireMock repository</a>.
+ *
+ * <p>
+ * 
+ * Each test method in this class corresponds to a specific scenario and tests the behavior of the AccessService based on the provided conditions.
+ * 
+ * <p>
+ * 
+ * The WireMockServer is used to start a mock HTTP server to simulate different server states and responses.
+ * 
+ * <p>
+ * 
+ * The {@link #setUp()} method starts the WireMock server and sets up stubs for HTTP endpoints.
+ * The {@link #tearDown()} method stops the WireMock server after each test.
+ */
 public class AccessServiceTest {
 
+    
     private WireMockServer wireMockServer;
 
+    /**
+     * Sets up the environment before each test by starting the WireMock server and configuring stubs for HTTP endpoints.
+     * This method is annotated with {@link BeforeEach}, ensuring that it runs before each test method in the test class.
+     *
+     * <p>
+     * 
+     * The WireMock server is started on port 8080, allowing it to simulate HTTP responses from a remote server.
+     * 
+     * <p>
+     *
+     * The stub for the health check endpoint is configured to return a 200 OK response, simulating a healthy server state.
+     */
     @BeforeEach
     public void setup() {
         // Start the WireMock server
@@ -28,6 +65,24 @@ public class AccessServiceTest {
                         .withStatus(200)));
     }
 
+    /**
+     * Tests the {@link AccessService} constructur to ensure that it initialize the {@link MachineTrackerAccessRemote} when the remote server is healthy.
+     * 
+     * This test case focuses on the scenario where:
+     * <ul>
+     *   <li>The remote server is healthy.</li>
+     * </ul>
+     * 
+     * <p>
+     * To conduct this test, we perform the following steps:
+     * </p>
+     * <ol>
+     *   <li>Arrange: Create an URI object for the remote server (mockedServerUri).</li>
+     *   <li>Act: Initialize a new AccessService object with mockedServerUri as URI and an arbitrary string as a filename </li>
+     *   <li>Assert: Assert that the AccessService initialices the {@link MachinetrackerAccessRemote} object</li>
+     * </ol>
+     * 
+     */
     @Test
     public void AccessService_testAccessServiceWithHealthyServer() {
         //Arrange
@@ -40,8 +95,29 @@ public class AccessServiceTest {
         assertTrue(accessService.getAccess() instanceof MachineTrackerAccessRemote, "Expected remote access");
     }
 
+    /**
+     * Tests the behavior of the AccessService when created with an unhealthy remote server.
+     * 
+     * <p>
+     * This test case focuses on the scenario where:
+     * </p>
+     * <ul>
+     *   <li>The remote server is unhealthy (returns a 500 status code).</li>
+     * </ul>
+     * 
+     * <p>
+     * To conduct this test, we perform the following steps:
+     * </p>
+     * <ol>
+     *   <li>Arrange: Set up a stub for the health check endpoint to return a 500 status code (unhealthy server).</li>
+     *   <li>Act: Initialize a new AccessService object with a URI for the mocked unhealthy server and an arbitrary filename.</li>
+     *   <li>Assert: Assert that the AccessService falls back to using {@link MachineTrackerAccessLocal}, indicating local access when the server is unhealthy.</li>
+     * </ol>
+     * 
+     */
     @Test
     public void AccessService_testAccessServiceWithUnhealthyServer() {
+       
         //Arrange
         stubFor(get(urlEqualTo("/health"))
                 .willReturn(aResponse()
@@ -55,6 +131,26 @@ public class AccessServiceTest {
         assertTrue(accessService.getAccess() instanceof MachineTrackerAccessLocal, "Expected local access");
     }
 
+    /**
+     * Tests the behavior of the AccessService when it encounters an exception during server communication and falls back to using local access.
+     * 
+     * <p>
+     * This test case focuses on the scenario where:
+     * </p>
+     * <ul>
+     *   <li>An exception occurs during server communication (malformed response chunk).</li>
+     * </ul>
+     * 
+     * <p>
+     * To conduct this test, we perform the following steps:
+     * </p>
+     * <ol>
+     *   <li>Arrange: Set up a stub for the health check endpoint to return a malformed response chunk.</li>
+     *   <li>Act: Initialize a new AccessService object with a URI for the mocked server and an arbitrary filename.</li>
+     *   <li> Assert: Assert that the AccessService falls back to using {@link MachineTrackerAccessLocal}, indicating local access after encountering an exception.</li>
+     * </ol>
+     * 
+     */
     @Test
     public void AccessService_testAccessServiceThrowsExceptionAndFallsBackToLocalAccess() {
         
@@ -70,9 +166,16 @@ public class AccessServiceTest {
         assertTrue(accessService.getAccess() instanceof MachineTrackerAccessLocal, "Expected local access after exception");
     }
     
+    /**
+     * Performs cleanup after each test by stopping the WireMock server used for simulating HTTP responses.
+     * 
+     * <p>
+     * To clean up, this method stops the WireMock server to ensure that resources are released and the server is shut down.
+     * </p>
+     * 
+    */
     @AfterEach
     public void tearDown() {
-        // Stop the WireMock server
         wireMockServer.stop();
     }
 }
