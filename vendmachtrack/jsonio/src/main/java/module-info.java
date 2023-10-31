17d48f868fef8419c08a0d22d47d6301c8e65773
Edward Storlien <carlst@stud.ntnu.no>
2023-10-31 19:36:20 +0100

    Issue #84: Update module-info files to strenghten encapsulation
    There is still a couple of 'opens' and 'exports' that is not directed towards any modules.
    This will be worked on more in a later release.

--- vendmachtrack/jsonio/src/main/java/module-info.java
+++ vendmachtrack/jsonio/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module vendmachtrack.jsonio {
+    // Allows the UI application to interact directly with the tracker file.
     exports vendmachtrack.jsonio.internal to vendmachtrack.ui;
+
+    // Weak encapsulation, but we haven't managed to find the unnamed module that is dependent on this module.
+    // This will be worked more on in a later release.
     exports vendmachtrack.jsonio;
 
     requires vendmachtrack.core;
