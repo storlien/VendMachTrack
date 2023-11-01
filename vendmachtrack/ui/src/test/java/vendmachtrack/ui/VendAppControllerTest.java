a1aa84221e369fc575497d73066b717f6d934e4f
Alexander Grannes <alexander.grannes@gmail.com>
2023-11-01 16:50:59 +0100

    Issue #100: Create and update Unit tests for VendAppController Class
    - Updated and created more Unit tests for the VendappController class
    - Added an the surefire plugin with an Argline to be able to run the
      Unit tests
     - Updated the module-info file to be able to run tests
    
     Will go over the tests with the team to make sure i have covered the
     most important functionallity.

--- vendmachtrack/ui/src/test/java/vendmachtrack/ui/VendAppControllerTest.java
+++ vendmachtrack/ui/src/test/java/vendmachtrack/ui/VendAppControllerTest.java
@@ -1,166 +1,294 @@
 package vendmachtrack.ui;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.stage.Stage;
+import vendmachtrack.ui.access.AccessService;
+import vendmachtrack.ui.access.MachineTrackerAccessible;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-import org.junit.jupiter.api.BeforeEach;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.matcher.control.TextInputControlMatchers;
-import vendmachtrack.core.MachineTracker;
-import vendmachtrack.core.VendingMachine;
-
-import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import java.util.HashMap;
 
 
 public class VendAppControllerTest extends ApplicationTest {
 
-
     private VendAppController controller;
-    private MachineTracker tracker;
-    private final MachineTracker trackerTest = new MachineTracker();
+    private HashMap<Integer, String> vendingMachines = new HashMap<>();
 
-    /**
-     * This method starts the application by loading the FXML file and setting up the stage.
-     *
-     * @param stage The stage to be used for the application.
-     * @throws Exception If there is an error loading the FXML file.
-     */
+    @Mock
+    private AccessService mockService;
+
+    @Mock
+    private MachineTrackerAccessible mockAccess;
+    
+
     @Override
     public void start(Stage stage) throws Exception {
-        // This uses the structure from the App class to start the application.
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("App.fxml")); // Make sure to use the correct path to FXML file.
+        // Initialize mocks
+        MockitoAnnotations.openMocks(this);
+
+        // Mock the behavior of the AccessService and MachineTrackerAccessible
+        when(mockService.getAccess()).thenReturn(mockAccess);
+        
+        // Mock a vending machine list for testing purposes
+        vendingMachines.put(1, "Oslo");
+        vendingMachines.put(2, "Trondheim");
+        when(mockAccess.getVendMachList()).thenReturn(vendingMachines);
+
+       
+
+        // Load the FXML and set the mocked service to the controller
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("App.fxml"));
         Parent parent = fxmlLoader.load();
         this.controller = fxmlLoader.getController();
+        this.controller.setAccessService(mockService);
 
-        //this.tracker = this.controller.getMachtrack();
+         controller.updateVendMachList();
 
-        stage.setOnCloseRequest((WindowEvent event) -> {
-            //this.controller.onClose();
-        });
-
+        // Set the scene and show the stage
         stage.setScene(new Scene(parent));
         stage.show();
     }
 
-
-    /**
-     * Sets up the test fixture before each test method runs.
-     * Initializes a VendingMachine object with a HashMap of product names and quantities,
-     * and adds it to the trackerTest object.
-     */
-    @BeforeEach
-    public void setup() {
-        //same as a vendingmachine in Tracker.Json (might read instead)
-        HashMap<String, Integer> status = new HashMap<>();
-        status.put("Cola", 5);
-        status.put("Pepsi", 3);
-        VendingMachine machine1 = new VendingMachine(1, status, "Trondhjem");
-
-        this.trackerTest.addVendingMachine(machine1);
-
-    }
-
-    /**
-     * Tests the VendAppController class.
-     * Ensures that the controller and tracker objects are not null.
-     */
     @Test
     public void testController_VenpAppController() {
         assertNotNull(this.controller);
-        assertNotNull(this.tracker);
+    }
+
+    @Test
+    public void VendAppController_testMenuBarPopulation() {
+        
+        //Arrange
+        controller.updateVendMachList();
+
+        //Act
+        ChoiceBox<String> menuBar = lookup("#menuBar").query();
+        clickOn("#menuBar");
+
+        //Assert
+        assertEquals(2, menuBar.getItems().size());
     }
 
 
-    /**
-     * Tests the vending machine controller by comparing the ID, location, and status of the vending machine
-     * in the test tracker with the vending machine in the actual tracker.
-     */
     @Test
-    public void testController_vendingMachine() {
-        assertEquals(trackerTest.getMachines().get(0).getId(), this.tracker.getMachines().get(0).getId());
-        assertEquals(trackerTest.getMachines().get(0).getLocation(), this.tracker.getMachines().get(0).getLocation());
-        assertEquals(trackerTest.getMachines().get(0).getStatus(), this.tracker.getMachines().get(0).getStatus());
-    }
+    public void VendAppController_testMenuBarPopulation_selectVendingMachine() {
+        
+        //Arrange
+        HashMap<String, Integer> mockInventory = new HashMap<>();
+        mockInventory.put("Cola", 5);
+        mockInventory.put("Pepsi", 3);
+        when(mockAccess.getInventory(1)).thenReturn(mockInventory);
 
-    /**
-     * Tests the vending machine selection functionality of the VendAppController class.
-     * Assumes that the ChoiceBox's items are populated at this point. Selects the first item in the ChoiceBox,
-     * clicks the "OK" button, and verifies that the TextArea contains the expected text for the first vending machine.
-     * Then, selects the second and third items in the ChoiceBox, clicks the "OK" button for each, and verifies that
-     * the TextArea contains the expected text for the corresponding vending machines.
-     */
-    @Test
-    public void testVendingMachineSelection() {
-        // Assuming the ChoiceBox's items are populated at this point,
-        // we'll select the first item in the ChoiceBox. 
-        ChoiceBox choiceBox = lookup("#menuBar").queryAs(ChoiceBox.class);
+        //Act
+        @SuppressWarnings("unchecked")
+        ChoiceBox<String> choiceBox = (ChoiceBox<String>) lookup("#menuBar").queryAs(ChoiceBox.class);
         interact(() -> choiceBox.getSelectionModel().select(0));
 
-        // Click the "OK" button.
-        clickOn("#button");
+        clickOn("#okButton");
 
-        // verify that the TextArea contains the expected text.
-        // Adjust the expected text based on the VendingMachine's status that you're testing.
+        //Assert
         TextArea textArea = lookup("#textArea").queryAs(TextArea.class);
         FxAssert.verifyThat(textArea, TextInputControlMatchers.hasText("Inventory:\nCola: 5\nPepsi: 3\n"));
-
-        // Second Vending Machine Selection and Verification
-
-        interact(() -> choiceBox.getSelectionModel().select(1)); // select the second item
-        clickOn("#button");
-
-        // Verify the TextArea for the second vending machine. 
-        // Change the expected text to match what's expected for your second machine.
-        FxAssert.verifyThat(textArea, TextInputControlMatchers.hasText("Inventory:\nTuborg: 1\n"));
-
-        interact(() -> choiceBox.getSelectionModel().select(2)); // select the second item
-        clickOn("#button");
-
-        // Verify the TextArea for the third vending machine. 
-        // Change the expected text to match what's expected for your second machine.
-        FxAssert.verifyThat(textArea, TextInputControlMatchers.hasText("Inventory:\nHansa: 100\nRegnvann: 10\n"));
     }
 
-    /**
-     * Tests the behavior of the "OK" button when no vending machine is selected in the ChoiceBox.
-     */
     @Test
-    public void testButtonClickWithNoSelection() {
-        // Deselect any currently selected item in the ChoiceBox.
-        ChoiceBox choiceBox = lookup("#menuBar").queryAs(ChoiceBox.class);
-        interact(() -> choiceBox.getSelectionModel().clearSelection());
+    public void VendAppController_testMenuBarPopulation_selectNoVendingMachine(){
+     
+        //Act
+        clickOn("#okButton");
 
-        // Click the "OK" button.
-        clickOn("#button");
-
-        // Verify the TextArea shows the "No vending machine selected" message.
+        //Assert
         TextArea textArea = lookup("#textArea").queryAs(TextArea.class);
         FxAssert.verifyThat(textArea, TextInputControlMatchers.hasText("No vending machine selected"));
     }
 
-    /**
-     * This method tests the existence of UI elements in the VendAppController.
-     * It checks if the Label, TextArea, Button and ChoiceBox elements exist.
-     * If any of the elements do not exist, the test will fail.
-     */
     @Test
-    public void testUIElementsExistence() {
-        assertNotNull(lookup("#myLabel").queryAs(Label.class));
-        assertNotNull(lookup("#textArea").queryAs(TextArea.class));
-        assertNotNull(lookup("#button").queryAs(Button.class));
-        assertNotNull(lookup("#menuBar").queryAs(ChoiceBox.class));
+    public void VendAppController_testRefill_noVendingMachineSelected(){
+
+        //Act
+        clickOn("#refillButton");
+
+        //Assert
+        TextArea textArea = lookup("#textArea").queryAs(TextArea.class);
+        FxAssert.verifyThat(textArea, TextInputControlMatchers.hasText("Please select a vending machine."));
     }
 
+    @Test
+    public void VendAppController_testUserview_noVendingMachineSelected(){
 
+        //Act
+        clickOn("#userView");
+
+        //Assert
+        TextArea textArea = lookup("#textArea").queryAs(TextArea.class);
+        FxAssert.verifyThat(textArea, TextInputControlMatchers.hasText("Please select a vending machine."));
+    }
+
+    @Test
+    public void VendAppController_testAddVendingMachine_validVendingMachine(){
+         
+        // Arrange
+        when(mockAccess.addVendMach(anyInt(), anyString())).thenAnswer(invocation -> {
+            int id = invocation.getArgument(0);
+            String location = invocation.getArgument(1);
+            vendingMachines.put(id, location);
+            return vendingMachines;
+        });
+
+        //Act
+        clickOn("#idTextFieldAdd").write("3");  // Add an ID for the new vending machine
+        clickOn("#locationTextField").write("Bergen");  // Add a location for the new vending machine
+        clickOn("#addButton");  // Click the add button to add the vending machine
+        ChoiceBox<String> menuBar = lookup("#menuBar").query();
+        Label outputTextLabel = lookup("#outputText").query();
+        
+
+        //Assert
+        assertEquals("The machine 3 was successfully added to your tracker ", outputTextLabel.getText());
+        assertEquals(3, menuBar.getItems().size());
+        assertTrue(menuBar.getItems().contains("id: 3 (Bergen)"));
+    }
+
+    @Test
+    public void VendAppController_testAddVendingMachine_ExistingID(){
+        
+        // Arrange
+        when(mockAccess.addVendMach(2, "Random Location"))
+        .thenThrow(new RuntimeException("Machine with ID: 2 already exists"));
+
+        // Act
+        clickOn("#idTextFieldAdd").write("2");  // Use the ID for the existing vending machine
+        clickOn("#locationTextField").write("Random Location");  // Add a different location for testing
+        clickOn("#addButton");  // Click the add button to attempt adding the vending machine
+        Label outputTextLabel = lookup("#outputText").query();
+
+        // Assert
+        assertEquals("Machine with ID: 2 already exists", outputTextLabel.getText());    
+    }
+
+    @Test
+    public void VendAppController_testAddVendingMachine_InvalidID(){
+        
+        // Act
+        clickOn("#idTextFieldAdd").write("NaN");  // Use the ID for the existing vending machine
+        clickOn("#locationTextField").write("Random Location");  // Add a different location for testing
+        clickOn("#addButton");  // Click the add button to attempt adding the vending machine
+        Label outputTextLabel = lookup("#outputText").query();
+        
+        // Assert
+        assertEquals("Machine ID must be an integer", outputTextLabel.getText());
+    }
+
+    
+    @Test
+    public void VendAppController_testAddVendingMachine_InvalidLocation(){
+        
+        // Arrange
+        when(mockAccess.addVendMach(5, "5"))
+        .thenThrow(new RuntimeException("Location Name not valid"));
+        
+        // Act
+        clickOn("#idTextFieldAdd").write("5");  // Use the ID for the existing vending machine
+        clickOn("#locationTextField").write("5");  // Add a different location for testing
+        clickOn("#addButton");  // Click the add button to attempt adding the vending machine
+        Label outputTextLabel = lookup("#outputText").query();
+        
+        // Assert
+        assertEquals("Location Name not valid", outputTextLabel.getText());
+    }
+
+       @Test
+    public void VendAppController_testAddVendingMachine_NoIdWritten(){
+
+        // Arrange
+        when(mockAccess.addVendMach(5, ""))
+        .thenThrow(new RuntimeException("Location Name not valid"));
+        
+        // Act
+        clickOn("#locationTextField").write("5");  
+        clickOn("#addButton");  
+        Label outputTextLabel = lookup("#outputText").query();
+        
+        // Assert
+        assertEquals("Machine ID must be an integer", outputTextLabel.getText());
+    }
+
+    @Test
+    public void VendAppController_testAddVendingMachine_NoLocationWritten(){
+        
+        // Arrange
+        when(mockAccess.addVendMach(5, ""))
+        .thenThrow(new RuntimeException("Location Name not valid"));
+
+        // Act
+        clickOn("#idTextFieldAdd").write("5");  // Use the ID for the existing vending machine
+        clickOn("#addButton");  // Click the add button to attempt adding the vending machine
+        Label outputTextLabel = lookup("#outputText").query();
+        
+        // Assert
+        assertEquals("Location Name not valid", outputTextLabel.getText());
+    }
+    
+    @Test
+    public void VendAppController_testRemoveVendingMachine_removesVendingmachine(){
+        
+        // Arrange
+        when(mockAccess.removeVendMach(1)).thenAnswer(invocation -> {
+            vendingMachines.remove(1);
+            return vendingMachines;
+        });
+
+        // Act
+        clickOn("#idTextFieldRemove").write("1");  // Use the ID for the existing vending machine
+        clickOn("#removeButton");  // Click the remove button to attempt removing the vending machine
+        Label outputTextLabel = lookup("#outputText").query();
+        ChoiceBox<String> menuBar = lookup("#menuBar").query();
+
+        // Assert
+        assertEquals("The machine 1 was successfully removed from your tracker", outputTextLabel.getText());
+        assertFalse(menuBar.getItems().contains("id: 1 (Oslo)"));
+    }
+    
+    @Test
+    public void VendAppController_testRemoveVendingMachine_invalidID(){
+
+        // Arrange
+        when(mockAccess.removeVendMach(5)).thenThrow(new RuntimeException("No such Vending Machine with ID: 5"));
+
+        // Act
+        clickOn("#idTextFieldRemove").write("5");  // Use the ID for the existing vending machine
+        clickOn("#removeButton");  // Click the remove button to attempt removing the vending machine
+        Label outputTextLabel = lookup("#outputText").query();
+
+        // Assert
+        assertEquals("No such Vending Machine with ID: 5", outputTextLabel.getText());
+    }
+
+    @Test
+    public void VendAppController_testRemoveVendingMachine_notInteger(){
+
+         // Act
+        clickOn("#idTextFieldRemove").write("NaN");  // Use the ID for the existing vending machine
+        clickOn("#removeButton");  // Click the remove button to attempt removing the vending machine
+        Label outputTextLabel = lookup("#outputText").query();
+
+        // Assert
+        assertEquals("Machine ID must be an integer", outputTextLabel.getText());
+    }
 }
